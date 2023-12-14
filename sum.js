e99d6fa892d04754448756494732d29e8b8935dd
fluflo <alex@trittibach.net>
2023-12-14 08:53:48 +0100

    Add first test case

---
+++ sum.js
@@ -0,0 +1,16 @@
+/**
+ * Returns the sum of two parameters
+ * @param {number} a The first summand
+ * @param {number} b The second summand
+ * 
+ */
+export function sum(a,b){
+    if (a === undefined){
+        a = 0;
+    }
+    if (b === undefined){
+        b = 0;
+    }
+    
+    return a + b
+}
